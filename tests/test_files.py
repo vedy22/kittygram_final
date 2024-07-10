e1c3f15577e285d72bc803ac6108360ffbda94c7
Tatyana <89541794+vedy22@users.noreply.github.com>
2024-07-11 00:06:08 +0300

    1

--- tests/test_files.py
+++ tests/test_files.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from typing import Union
 
 import yaml
 
@@ -20,7 +19,7 @@ def test_deploy_info_file_content(
         deploy_file_info: tuple[Path, str],
         deploy_info_file_content: dict[str, str],
         expected_deploy_info_file_content: dict[str, str]
-):
+        ):
     _, relative_path = deploy_file_info
     missing_content = {
         key: value for key, value in expected_deploy_info_file_content.items()
@@ -50,29 +49,12 @@ def test_backend_dokerfile_content(backend_dir_info: tuple[Path, str],
     path, _ = backend_dir_info
     with open(path / dockerfile_name, encoding='utf-8', errors='ignore') as f:
         dockerfile_content = f.read()
-    expected_keywords = (
-        'from', 'run',
-        'cmd' if 'cmd' in dockerfile_content.lower() else 'entrypoint'
-    )
+    expected_keywords = ('from', 'run', 'cmd')
     for keyword in expected_keywords:
         assert keyword in dockerfile_content.lower(), (
             f'Убедитесь, что настроили {dockerfile_name} для образа '
             '`kittygram_backend`.'
         )
-
-
-def safely_load_yaml_file(path_to_file: Path) -> dict[str, Union[dict, str]]:
-    with open(path_to_file, 'r', encoding='utf-8', errors='ignore') as stream:
-        try:
-            file_content = yaml.safe_load(stream)
-        except yaml.YAMLError as exc:
-            raise AssertionError(
-                f'Убедитесь, что в файле `{path_to_file}` используется '
-                'корректный YAML-синтаксис. При попытке чтения файла возникло '
-                'исключение:\n'
-                f'{exc.__class__.__name__}: {exc}'
-            )
-    return file_content
 
 
 def test_workflow_file(base_dir: Path, workflow_file_name: str):
@@ -81,7 +63,16 @@ def test_workflow_file(base_dir: Path, workflow_file_name: str):
         f'Убедитесь, что корневая директория проекта содержит файл '
         f'`{workflow_file_name}`, в котором описан workflow для Kittygram.'
     )
-    workflow = safely_load_yaml_file(path_to_file)
+    with open(path_to_file, 'r', encoding='utf-8', errors='ignore') as stream:
+        try:
+            workflow = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            raise AssertionError(
+                f'Убедитесь, что в файле `{workflow_file_name}` используется '
+                'корректный YAML-синтаксис. При попытке чтения файла возникло '
+                'исключение:\n'
+                f'{exc.__class__.__name__}: {exc}'
+            )
     assert workflow, (
         f'Убедитесь, что файл `{workflow_file_name}` в корневой директории '
         'проекта содержит настройку workflow проекта.'
@@ -95,34 +86,4 @@ def test_requirements_location(backend_dir_info: tuple[Path, str]):
     assert path_to_file.is_file(), (
         f'Убедитесь, что директория {relative_backend_path} содержит файл '
         f'зависимостей `{requirements_file_name}`.'
-    )
-
-
-def has_forbiden_keyword(file_content: dict[str, Union[dict, str]],
-                         forbidden_keyword: str) -> bool:
-    is_forbidden_keyword_used = False
-    for key, value in file_content.items():
-        if isinstance(value, dict):
-            if has_forbiden_keyword(value, forbidden_keyword):
-                is_forbidden_keyword_used = True
-        if key == forbidden_keyword:
-            return True
-    return is_forbidden_keyword_used
-
-
-def test_docker_compose_prod_file_exists(base_dir: Path,
-                                         docker_compose_prod_file_name: str):
-    path_to_file = base_dir / docker_compose_prod_file_name
-    assert path_to_file.is_file(), (
-        f'Убедитесь, что корневая директория проекта содержит файл '
-        f'`{docker_compose_prod_file_name}`.'
-    )
-    compose = safely_load_yaml_file(path_to_file)
-    assert compose, (
-        f'Убедитесь, что файл `{docker_compose_prod_file_name}` в корневой '
-        'директории проекта содержит конфигурацию запуска проекта.'
-    )
-    assert not has_forbiden_keyword(compose, 'build'), (
-        f'Убедитесь, что файл `{docker_compose_prod_file_name}` не содержит '
-        'инструкции `build`.'
     )
