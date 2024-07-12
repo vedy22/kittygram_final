60a6cfccceb2ccbb38905b4661b3dfb7f3cc3b1d
Tatyana <89541794+vedy22@users.noreply.github.com>
2024-07-12 17:57:10 +0300

    Update manage.py

--- backend/manage.py
+++ backend/manage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# flake8: noqa
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
@@ -6,8 +7,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                          'kittygram_backend.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kittygram_backend.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
